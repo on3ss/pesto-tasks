6c3bb33f7f2d893373820367e9138c52239fc2d8
on3ss <auditoree9@gmail.com>
2024-06-04 20:31:27 +0530

    refactored for separation of modal

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,42 +1,74 @@
+import { useMemo } from "react"
 import Navbar from "../components/Navbar"
 
 function Home() {
-    const handleOnClickModal = () => document.getElementById('create-form-modal').showModal()
+    const taskFormModalID = useMemo(() => 'create-form-modal', [])
+    const showTaskFormModal = () => {
+        const modal = document.getElementById(taskFormModalID) as HTMLDialogElement
+        if (modal) {
+            modal.showModal()
+        }
+    }
+
     return (
-        <div className="">
-            <Navbar />
-            <main>
-                <section className="mx-2 my-4 rounded bg-primary-content">
-                    <div className="flex justify-center">
-                        <button className="btn btn-link focus:outline-none" onClick={handleOnClickModal}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                            </svg>
-                            <span>Add New Task</span>
-                        </button>
-                        <dialog id="create-form-modal" className="modal modal-bottom sm:modal-middle">
-                            <div className="modal-box">
-                                <h3 className="text-lg font-bold">Hello!</h3>
-                                <p className="py-4">Press ESC key or click the button below to close</p>
-                                <div className="modal-action">
-                                    <form method="dialog">
-                                        {/* if there is a button in form, it will close the modal */}
-                                        <button className="btn">Close</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </dialog>
-                    </div>
-                    <ul>
-                        <TaskListItem />
-                        <TaskListItem />
-                        <TaskListItem />
-                        <TaskListItem />
-                        <TaskListItem />
-                    </ul>
-                </section>
-            </main>
-        </div>
+        <>
+            <div className="">
+                <Navbar />
+                <main>
+                    <section className="mx-2 my-4 rounded bg-primary-content">
+                        <div className="flex justify-center">
+                            <button className="btn btn-link focus:outline-none" onClick={showTaskFormModal}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+                                <span>Add New Task</span>
+                            </button>
+                        </div>
+                        <ul>
+                            <TaskListItem />
+                            <TaskListItem />
+                            <TaskListItem />
+                            <TaskListItem />
+                            <TaskListItem />
+                        </ul>
+                    </section>
+                </main>
+            </div>
+            <TaskFormModal taskFormModalID={taskFormModalID} />
+        </>
+    )
+}
+
+function TaskFormModal({ taskFormModalID }: { taskFormModalID: string }) {
+    return (
+        <dialog id={taskFormModalID} className="modal modal-bottom sm:modal-middle">
+            <div className="modal-box">
+                <h3 className="text-lg font-bold">Add Task</h3>
+                <form action="">
+                    <label className="w-full my-4 form-control">
+                        <div className="label">
+                            <span className="label-text">Name</span>
+                        </div>
+                        <input type="text" placeholder="Name" className="w-full input input-bordered" />
+                    </label>
+                    <label className="my-4 form-control">
+                        <div className="label">
+                            <span className="label-text">Description</span>
+                        </div>
+                        <textarea className="h-24 textarea textarea-bordered" placeholder="Description"></textarea>
+                    </label>
+                    <button className="btn btn-primary float-end">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        <span>Add</span>
+                    </button>
+                </form>
+            </div>
+            <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
     )
 }
 
