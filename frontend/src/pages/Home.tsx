00b765763263284e29512cf2f5ba1c2a5866e6ed
Oness Office WSL <auditoree9@gmail.com>
2024-06-04 17:10:31 +0530

    task card and modal

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,62 +1,74 @@
 import Navbar from "../components/Navbar"
 
 function Home() {
+    const handleOnClickModal = () => document.getElementById('create-form-modal').showModal()
     return (
         <div className="">
             <Navbar />
             <main>
                 <section className="mx-2 my-4 rounded bg-primary-content">
                     <div className="flex justify-center">
-                        <button className="btn btn-link">
+                        <button className="btn btn-link focus:outline-none" onClick={handleOnClickModal}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                             </svg>
                             <span>Add New Task</span>
                         </button>
-                    </div>
-                    <ul>
-                        <li className="border-t-[1px] border-base-100 px-2 py-4">
-                            <div className="flex items-center justify-between">
-                                <h5 className="text-lg font-semibold">Task 1</h5>
-                                <div className="dropdown dropdown-end">
-                                    <div tabIndex={0} role="button" className="m-1 btn btn-ghost">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
-                                        </svg>
-                                    </div>
-                                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
-                                        <li>
-                                            <button className="flex w-full btn btn-ghost">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                                                    <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                                </svg>
-                                                <span>Item 1</span>
-                                            </button>
-                                        </li>
-                                        <li>
-                                            <a className="btn btn-ghost text-nowrap">Item 2</a>
-                                        </li>
-                                    </ul>
+                        <dialog id="create-form-modal" className="modal modal-bottom sm:modal-middle">
+                            <div className="modal-box">
+                                <h3 className="text-lg font-bold">Hello!</h3>
+                                <p className="py-4">Press ESC key or click the button below to close</p>
+                                <div className="modal-action">
+                                    <form method="dialog">
+                                        {/* if there is a button in form, it will close the modal */}
+                                        <button className="btn">Close</button>
+                                    </form>
                                 </div>
                             </div>
-                            <p className="line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae beatae ab consequatur aut numquam sed mollitia, odio repellendus magni quaerat.</p>
-                            <div className="flex items-center justify-start gap-1 my-2">
-                                <span className="px-2 py-1 text-sm font-semibold rounded bg-info text-info-content">Tag1</span>
-                                <span className="px-2 py-1 text-sm font-semibold rounded bg-info text-info-content">Tag2</span>
-                            </div>
-                        </li>
-                        <li className="border-t-[1px] border-base-100 px-2 py-4">
-                            <h5 className="text-lg font-semibold">Task 1</h5>
-                            <p className="line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae beatae ab consequatur aut numquam sed mollitia, odio repellendus magni quaerat.</p>
-                            <div className="flex items-center justify-start gap-1 my-2">
-                                <span className="px-2 py-1 text-sm font-semibold rounded bg-info text-info-content">Tag1</span>
-                                <span className="px-2 py-1 text-sm font-semibold rounded bg-info text-info-content">Tag2</span>
-                            </div>
-                        </li>
+                        </dialog>
+                    </div>
+                    <ul>
+                        <TaskListItem />
+                        <TaskListItem />
+                        <TaskListItem />
+                        <TaskListItem />
+                        <TaskListItem />
                     </ul>
                 </section>
             </main>
         </div>
+    )
+}
+
+function TaskListItem() {
+    return (
+        <li className="card-body border-t-[1px] border-base-100">
+            <div className="flex items-center justify-start gap-2">
+                <h2 className="card-title">Info Card Title</h2>
+                <span className="text-xs badge badge-info badge-outline">Pending</span>
+            </div>
+            <p>This is a description of the info card. It contains relevant information that you want to display.</p>
+            <div className="justify-start card-actions">
+                <button className="btn btn-sm btn-success">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                    <span>Complete</span>
+                </button>
+                <button className="btn btn-sm btn-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>
+                    <span>Edit</span>
+                </button>
+                <button className="btn btn-sm btn-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                    <span>Delete</span>
+                </button>
+            </div>
+        </li>
     )
 }
 
