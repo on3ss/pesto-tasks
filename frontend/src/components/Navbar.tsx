e2d3058adc6164cc08b522982b59b238b24f41d6
on3ss <auditoree9@gmail.com>
2024-06-04 21:48:13 +0530

    changed header and added avatar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,8 +2,23 @@ function Navbar() {
     return (
         <header className="fixed top-0 z-50 navbar bg-primary text-primary-content">
             <div className="flex-1">
-                <a className="text-xl btn btn-ghost">TaskManager</a>
+                <a className="text-xl btn btn-ghost">taskMN</a>
             </div>
+            <nav className="flex-none gap-2">
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                        </div>
+                    </div>
+                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-300 rounded-box w-52 text-base-content">
+                        <li>
+                            <a>Profile</a>
+                        </li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     )
 }
