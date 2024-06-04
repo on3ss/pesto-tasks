95346379e231be75541243a2f1b9f0d01ebf65de
on3ss <auditoree9@gmail.com>
2024-06-04 21:41:03 +0530

    added search and filter inputs

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -16,6 +16,19 @@ function Home() {
         <>
             <Navbar />
             <main className="relative mt-24">
+
+                <div className="flex items-center justify-between gap-2 mx-2 mb-4">
+                    <label className="flex items-center w-full gap-2 input input-bordered">
+                        <input type="text" className="grow" placeholder="Search" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-6 h-6 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                    </label>
+                    <button className="btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
+                        </svg>
+                    </button>
+                </div>
+
                 <section className="mx-2 mb-4 rounded bg-primary-content">
                     <div className="flex justify-center">
                         <button className="btn btn-link focus:outline-none" onClick={showTaskFormModal}>
