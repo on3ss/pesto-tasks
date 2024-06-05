56eca95cce9cc8c0cf0e5ceb7b342a49d79c0422
Oness Office WSL <auditoree9@gmail.com>
2024-06-05 12:03:12 +0530

    added sort modal

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar"
 import TaskFormModal from "../components/TaskFormModal"
 import TaskListItem from "../components/TaskListItem"
 import TaskFilterModal from "../components/TaskFilterModal"
+import TaskSortModal from "../components/TaskSortModal"
 
 function Home() {
     const taskFormModalID = useMemo(() => 'task-form-modal', [])
@@ -21,23 +22,35 @@ function Home() {
         }
     }
 
-
+    const taskSortModalID = useMemo(() => 'task-sort-modal', [])
+    const showTaskSortModal = () => {
+        const modal = document.getElementById(taskSortModalID) as HTMLDialogElement
+        if (modal) {
+            modal.showModal()
+        }
+    }
 
     return (
         <>
             <Navbar />
             <main className="relative mt-24">
-
                 <div className="flex items-center justify-between gap-2 mx-2 mb-4">
                     <label className="flex items-center w-full gap-2 input input-bordered">
                         <input type="text" className="grow" placeholder="Search" />
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-6 h-6 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
                     </label>
-                    <button className="btn" onClick={showTaskFilterModal}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
-                        </svg>
-                    </button>
+                    <div className="join">
+                        <button className="btn join-item" onClick={showTaskFilterModal}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z" />
+                            </svg>
+                        </button>
+                        <button className="btn join-item" onClick={showTaskSortModal}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
 
                 <section className="mx-2 mb-4 rounded bg-primary-content">
@@ -54,6 +67,7 @@ function Home() {
             </main>
             <TaskFormModal taskFormModalID={taskFormModalID} />
             <TaskFilterModal taskFilterModalID={taskFilterModalID} />
+            <TaskSortModal taskSortModalID={taskSortModalID} />
         </>
     )
 }
