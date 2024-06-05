8b92094c39beb344aaa00d3937d7463c9ca367af
Onessimus Lyngdoh <auditoree9@gmail.com>
2024-06-06 00:33:52 +0530

    removed icons

--- frontend/src/components/TaskFilterModal.tsx
+++ frontend/src/components/TaskFilterModal.tsx
@@ -20,10 +20,7 @@ function TaskFilterModal({ taskFilterModalID }: { taskFilterModalID: string }) {
                     </select>
                 </label>
                 <button className="btn btn-primary float-end">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                    </svg>
-                    <span>Filter</span>
+                    <span>Apply</span>
                 </button>
             </form>
         </Modal>
