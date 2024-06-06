5583cfa3c0dfbf215406623f73711b5de1d56e9f
Onessimus Lyngdoh <auditoree9@gmail.com>
2024-06-07 00:14:02 +0530

    responsive search box

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,25 +1,25 @@
 function Navbar() {
     return (
         <header className="fixed inset-x-0 top-0 z-50 w-full bg-primary text-primary-content">
-            <div className="container mx-auto navbar">
-                <div className="flex-1">
-                    <a className="text-xl btn btn-ghost">taskMN</a>
-                </div>
-                <nav className="flex-none gap-2">
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                            </div>
-                        </div>
-                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-300 rounded-box w-52 text-base-content space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-10">
-                            <li>
-                                <a>Profile</a>
-                            </li>
-                            <li><a>Logout</a></li>
-                        </ul>
+            <div className="container mx-auto">
+                <div className="navbar">
+                    <div className="flex-1">
+                        <a className="text-xl btn btn-ghost">taskMN</a>
                     </div>
-                </nav>
+                    <nav className="flex-none gap-2">
+                        <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                </div>
+                            </div>
+                            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-300 rounded-box w-52 text-base-content space-x-2 sm:space-x-4 md:space-x-6 lg:space-x-8 xl:space-x-10">
+                                <li><a>Profile</a></li>
+                                <li><a>Logout</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </header>
     )
--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -24,7 +24,7 @@ function Search() {
     }, [queryParams]);
 
     return (
-        <label className="flex items-center w-full gap-2 md:w-1/2 lg:w-1/4 input input-bordered">
+        <label className="flex items-center w-full gap-2 input input-bordered md:w-72 lg:w-96">
             <input type="text" className="grow" placeholder="Search" value={search} onChange={handleSearchChange} />
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-6 h-6 opacity-70">
                 <path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" />
