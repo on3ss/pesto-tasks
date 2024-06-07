7c37848ba052283a31240f1ccb29bf60fa289ea1
Oness Office WSL <auditoree9@gmail.com>
2024-06-07 16:37:13 +0530

    remove complete button

--- frontend/src/components/TaskListItem.tsx
+++ frontend/src/components/TaskListItem.tsx
@@ -9,12 +9,6 @@ function TaskListItem({ task }: { task: Task }) {
             </div>
             <p>{task.description ?? 'No Description'}</p>
             <div className="justify-start card-actions">
-                <button className="btn btn-sm btn-success">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
-                    <span>Complete</span>
-                </button>
                 <button className="btn btn-sm btn-primary">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
