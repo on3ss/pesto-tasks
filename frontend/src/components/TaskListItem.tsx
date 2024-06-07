c3e6380d988bafa39a60d2f89458687d03621ac8
Onessimus Lyngdoh <auditoree9@gmail.com>
2024-06-08 01:04:22 +0530

    implemented edit and delete

--- frontend/src/components/TaskListItem.tsx
+++ frontend/src/components/TaskListItem.tsx
@@ -1,31 +1,150 @@
-import { Task } from "../types"
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { FormValues, Task } from "../types";
+import apiUtil from '../utils/apiUtil';
+import { useMutation, useQueryClient } from 'react-query';
+import { useStatus } from '../contexts/StatusContext';
+
+const schema = yup.object({
+    name: yup.string().required().min(4).max(255),
+    description: yup.string().nullable().max(1000),
+    status_id: yup.number().required(),
+}).required();
 
 function TaskListItem({ task }: { task: Task }) {
-    return (
-        <>
-            <li className="card-body border-t-[1px] last:border-b-[1px] border-base-100">
-                <div className="flex items-start justify-between gap-2">
-                    <h2 className="card-title">{task.name}</h2>
-                    <span className={`text-xs badge badge-${task.status.theme_color ?? 'info'} badge-outline text-nowrap`}>{task.status.name}</span>
-                </div>
-                <p>{task.description ?? 'No Description'}</p>
-                <div className="justify-start card-actions">
-                    <button className="btn btn-sm btn-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                        </svg>
-                        <span>Edit</span>
-                    </button>
-                    <button className="btn btn-sm btn-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                        </svg>
-                        <span>Delete</span>
-                    </button>
-                </div>
-            </li>
-        </>
+    const [isEditing, setIsEditing] = useState(false);
+    const { statuses } = useStatus()
+    const { register, handleSubmit, formState: { errors }, setError } = useForm<FormValues>({
+        resolver: yupResolver(schema),
+        defaultValues: {
+            ...task,
+            status_id: task.status.id
+        }
+    });
+
+    const queryClient = useQueryClient()
+
+    const updateMutation = useMutation(
+        (updatedTask: FormValues) => apiUtil.patch(`/api/task/${task.id}`, updatedTask),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries(['tasks'])
+                setIsEditing(false)
+            },
+            onError: (error: any) => {
+                if (error.response?.data?.errors) {
+                    const errorData = error.response.data.errors;
+                    Object.keys(errorData).forEach((key) => {
+                        setError(key as keyof FormValues, {
+                            type: 'server',
+                            message: errorData[key][0], // Assuming the server sends the first error message only
+                        });
+                    });
+                } else {
+                    alert('Failed to add task.');
+                }
+            }
+        }
     )
+
+    const onSubmit: SubmitHandler<FormValues> = async (data) => {
+        updateMutation.mutate(data)
+    };
+
+    const deleteMutation = useMutation(
+        () => apiUtil.delete(`/api/task/${task.id}`),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries()
+            },
+            onError: () => alert('Something went wrong! Could not delete task')
+        }
+    )
+
+    const handleDelete = async () => {
+        if (window.confirm('Are you sure you want to delete this task?')) {
+            deleteMutation.mutate()
+        }
+    };
+
+    return (
+        <li className="card-body border-t-[1px] last:border-b-[1px] border-base-100">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {isEditing ? (
+                    <>
+                        <label className="w-full my-4 form-control">
+                            <div className="label">
+                                <span className="label-text">Name</span>
+                            </div>
+                            <input type="text" placeholder="Name" className="w-full input input-bordered" {...register('name')} />
+                            <div className="label">
+                                <span className="label-text-alt text-error">{errors.name?.message}</span>
+                            </div>
+                        </label>
+                        <label className="my-4 form-control">
+                            <div className="label">
+                                <span className="label-text">Description</span>
+                            </div>
+                            <textarea className="h-24 textarea textarea-bordered" placeholder="Description" {...register('description')}></textarea>
+                            <div className="label">
+                                <span className="label-text-alt text-error">{errors.description?.message}</span>
+                            </div>
+                        </label>
+                        <label className="my-4 form-control">
+                            <div className="label">
+                                <span className="label-text">Status</span>
+                            </div>
+                            <select className="select select-bordered" {...register('status_id')}>
+                                {statuses?.map((status) => (
+                                    <option key={status.id} value={status.id}>
+                                        {status.name}
+                                    </option>
+                                ))}
+                            </select>
+                            <div className="label">
+                                <span className="label-text-alt text-error">{errors.status_id?.message}</span>
+                            </div>
+                        </label>
+                    </>
+                ) : (
+                    <>
+                        <div className="flex items-start justify-between gap-2">
+                            <h2 className="card-title">{task.name}</h2>
+                            <span className={`text-xs badge badge-${task.status.theme_color ?? 'info'} badge-outline text-nowrap`}>{task.status.name}</span>
+                        </div>
+                        <p>{task.description ?? 'No Description'}</p>
+                    </>
+                )}
+                <div className="justify-start my-4 card-actions">
+                    {isEditing ? (
+                        <>
+                            <button type="submit" className="btn btn-sm btn-primary">
+                                Save
+                            </button>
+                            <button className="btn btn-sm btn-error" role='button' onClick={(e) => {
+                                e.preventDefault()
+                                setIsEditing(false)
+                            }}>
+                                Cancel
+                            </button>
+                        </>
+                    ) : (
+                        <button className="btn btn-sm btn-primary" role='button' onClick={(e) => {
+                            e.preventDefault()
+                            setIsEditing(true)
+                        }}>
+                            Edit
+                        </button>
+                    )}
+                    <button type="button" className="btn btn-sm btn-secondary" onClick={handleDelete}>
+                        Delete
+                    </button>
+                </div>
+            </form>
+        </li >
+    );
 }
 
-export default TaskListItem
+export default TaskListItem;
