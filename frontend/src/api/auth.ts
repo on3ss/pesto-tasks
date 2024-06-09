75dc5db9f436a2fd9870b34e5451afeace81b501
Onessimus Lyngdoh <auditoree9@gmail.com>
2024-06-09 22:25:29 +0530

    removed console.log

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -16,6 +16,5 @@ export const logoutUser = async () => {
 
 export const getUserData = async () => {
     const response = await apiUtil.get(`/api/user`);
-    console.log(response)
     return response.data;
 };
