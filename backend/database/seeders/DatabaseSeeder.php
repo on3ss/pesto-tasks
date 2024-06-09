4f2e362b39b900de0b8bf329fcd2ebffe9ba8ddc
Onessimus Lyngdoh <auditoree9@gmail.com>
2024-06-09 22:43:04 +0530

    added readme

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             StatusSeeder::class,
-            TaskSeeder::class,
+            // TaskSeeder::class,
         ]);
     }
 }
